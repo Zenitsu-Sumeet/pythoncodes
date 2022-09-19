fadc265f408b6907f6061d1bcbb426709602a39a
pc1 <thesumeetpay@gmail.com>
2022-09-19 12:22:46 +0530

    yjh

---
+++ classofstudents.py
@@ -0,0 +1,17 @@
+class Students():
+    def sumeet(sname, srollno, smobile, scourse):
+        sname = input("enter the name ")
+        rollno = int(input("enter the roll no "))
+        smobile = int(input("enter them mobile number "))
+        scourse = input("enter the courses ")
+        print(sname)
+        print(rollno)
+        print(smobile)
+        print(scourse)
+s = Students
+a=0
+b=0
+c=0
+d=0
+Students.sumeet(a, b, c, d)
+
