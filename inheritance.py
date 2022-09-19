8a73da739e86fd08ff6f4c7d91bfaa3538c94bcc
pc1 <thesumeetpay@gmail.com>
2022-09-19 12:25:26 +0530

    yjh

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class A:
+    def a():
+        print("Hello Students")
+class B(A):
+    def b():
+        print(" Welcome")
+class C(B):
+    def c():
+        print(" TO second home")
+c1=C
+c1.b()
+c1.a()
+c1.c()
