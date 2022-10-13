50d4d00f47af30f89d3cfd5f658ab6a082db6088
SCMS <thesumeetpay@gmail.com>
2022-10-13 14:29:50 +0530

    kghjkg

---
+++ kinter.py
@@ -0,0 +1,10 @@
+
+from tkinter import*
+
+window =Tk()
+window.title("9a")
+window.geometry("300x300")
+mylabel = Label(window,text="sumeet",bg="red",fg="white",font=('times',18),width=20)
+mylabel.pack()
+
+window.mainloop()
