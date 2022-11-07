7f0be61fec633bf69faff43438a9414d21039995
SCMS <thesumeetpay@gmail.com>
2022-11-07 14:08:20 +0530

    fibn

--- paldrime.py
+++ paldrime.py
@@ -1,8 +1,26 @@
 def paldrinome(n):
-    return n[::-1]
+    return n==n[::-1]
+
+n ='gfg'
+ans = paldrinome(n);
+if ans:
+    print("yes")
+else:
+    print("no")
+
+def number(s):
+    temp=s
+    r=0
+    while s>0:
+        s = temp/10
+        r=r*10+r
+        temp=temp//10
+    if n==r:
+        print("yes")
+    else:
+        print("no")
 
 
-def pladrime(n):
- temp = n
- rev =0
-  while()
+number(548)
+
+
